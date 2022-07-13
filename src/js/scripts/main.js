2a7599ea77b445c5f07c9e989a6acfa08a18770b
Myke <myke.programacao@gmail.com>
2022-07-13 19:08:30 -0300

    feat: evento no bloco de navegação foi colocado, para os botoes de filtro todos e concluidos no controller/filtroMain

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -1,6 +1,9 @@
 import tabelaEvent from "../controller/tabelaEvento.controller.js"
 import HabitosAPI from "../controller/renderizarHabitos.controller.js"
+import FiltroMain from "../controller/filtroMain.controller.js"
 
 const botoesTabela = new tabelaEvent()
+
+const navegacaoTabela = new FiltroMain()
 
 HabitosAPI.renderiarHabitos()
