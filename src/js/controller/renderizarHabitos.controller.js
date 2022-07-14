0962779fc7a50624d38d6cfa1a40aac91b6dddeb
Myke <myke.programacao@gmail.com>
2022-07-14 19:21:36 -0300

    feat:botao carregar mais da main, carregando de 10 em 10 cada fez que click a mais de post

--- src/js/controller/renderizarHabitos.controller.js
+++ src/js/controller/renderizarHabitos.controller.js
@@ -5,23 +5,34 @@ export default class HabitosAPI {
 
     static tabela = document.querySelector(".tabela__body")
 
+    static quantidadeRenderizar = 10
+
     static async renderiarHabitos(){
 
-        const todosHabitos = await HabitRequest.readAll()
+        const todosHabitos = ( await HabitRequest.readAll() ).sort( ( a, b ) => b.habit_id - a.habit_id )
+
+        const ordenarDadosdez = todosHabitos.slice(0, HabitosAPI.quantidadeRenderizar )
 
         HabitosAPI.tabela.innerHTML = ""
         
-        todosHabitos.forEach( Habito.criar )
+        ordenarDadosdez.forEach( Habito.criar )
     }
 
     static async renderizarHabitosConcluidos(){
 
-        const todosHabitos = await HabitRequest.readAll()
-        
+        const todosHabitos = ( await HabitRequest.readAll() ).sort( ( a, b ) => b.habit_id - a.habit_id )
+
         const habitosConcluidos = todosHabitos.filter( objHabito => objHabito.habit_status === true )
+
+        const ordenarDadosdez = habitosConcluidos.slice(0, HabitosAPI.quantidadeRenderizar )
 
         HabitosAPI.tabela.innerHTML = ""
 
-        habitosConcluidos.forEach( Habito.criar )
+        ordenarDadosdez.forEach( Habito.criar )
+    }
+
+    static async quantidadeRenderizacao(){
+
+        HabitosAPI.quantidadeRenderizar += 10
     }
 }
