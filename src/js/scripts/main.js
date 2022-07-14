d9423e389e22cb76e80ef61a094ebb50a38c3f35
EryclesSousa <erycles.sousa@gmail.com>
2022-07-14 18:00:25 -0300

    feat: cursosr pointer adicionado aos botões da main

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -3,6 +3,7 @@ import HabitosAPI from "../controller/renderizarHabitos.controller.js"
 import FiltroMain from "../controller/filtroMain.controller.js"
 import EditarHabito from "../controller/modalEditarHabito.controller.js"
 import ModalExcluir from "../controller/modalExcluir.controller.js"
+import PegarInfHabito from "../controller/pegarInfHabito.js"
 import LoginMain from "../controller/loginMain.controller.js"
 
 const botoesTabela = new tabelaEvent()
@@ -12,6 +13,8 @@ const navegacaoTabela = new FiltroMain()
 const navegacaoModalEditar = new EditarHabito()
 
 const navegacaoModalExcluir = new ModalExcluir()
+
+const navegacaoModalCriarHabite = new PegarInfHabito()
 
 HabitosAPI.renderiarHabitos()
 
